865291e61cea4935febe32cff2c11ae33060ee4e
i has candys <ichen10@stuy.edu>
2020-10-31 22:53:04 -0400

    Triangle class and getPerimeter added

--- Point.java
+++ Point.java
@@ -23,5 +23,4 @@ public class Point{
 	public boolean equals(Point other) {
 		return (this.x == other.x && this.y == other.y);
 	}
-
 }
--- Tester.java
+++ Tester.java
@@ -1,10 +1,23 @@
 public class Tester {
 	public static void main(String[] args) {
-		Point A = new Point(0, 0);
-		Point B = new Point(3, 4);
-		Point C = new Point(0, 0);
-		System.out.println(A.distanceTo(B));
-		System.out.println(A.equals(B));
-		System.out.println(A.equals(C));
+		Point a = new Point(0, 0);
+		Point b = new Point(3, 4);
+		Point c = new Point(0, 0);
+		System.out.println(a.distanceTo(b));
+		System.out.println(a.equals(b));
+		System.out.println(a.equals(c));
+
+		System.out.println();
+
+		Point d = new Point(0 , 4);
+		Triangle x = new Triangle(a, b, d); // perimeter = 12; area = 6; scalene
+		Triangle y = new Triangle(-5, 0, 9, 0, 0, 12); // perimeter = 42; area = 84; scalene
+		Triangle z = new Triangle(1, 0, -1, 0, 0, Math.sqrt(3)); // perimeter = 6; area = sqrt(3); equilateral
+		Triangle alpha = new Triangle(0, 3, 0, -3, 4, 0); // perimeter = 16; area = 12; isosceles
+		
+		System.out.println(x.getPerimeter());
+		System.out.println(y.getPerimeter());
+		System.out.println(z.getPerimeter());
+		System.out.println(alpha.getPerimeter());
     }
 }
